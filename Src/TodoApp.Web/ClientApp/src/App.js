e72b6a2e3dbd57f575580a765a6cf612b2c678f9
Khurshid Ali <md_khurshid@hotmail.com>
2022-08-13 18:57:03 -0300

    cleanup

--- Src/TodoApp.Web/ClientApp/src/App.js
+++ Src/TodoApp.Web/ClientApp/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <Container fluid>
+      <Container fluid="md">
         <Row>
           <Col>
           <TodoContainer></TodoContainer>  
